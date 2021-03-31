880e0fa0632c31ed56dbcce2527fcf626c9dd9ad
Suikei Wang <Suikei.Wong@anu.edu.au>
2021-03-31 12:20:14 +1100

    Update feature-map.py

--- feature-map.py
+++ feature-map.py
@@ -10,13 +10,11 @@ model, input_size = initialize_model(num_classes=5)
 model.load_state_dict(torch.load('weights/model5', map_location=torch.device('cpu')))
 model = torch.nn.Sequential(*(list(model.children())[:-1]))
 
-
 # test an image
 test = cv2.imread('separated-data/test/im_Koilocytotic/001.bmp')
 dim = (input_size, input_size)
 test = cv2.resize(test, dim)
 test = torch.Tensor(test)
-
 def feature_visualization(model, img):
     '''prints the cat as a 2d array'''
     img_batch = np.expand_dims(img,axis=0)
@@ -25,11 +23,7 @@ def feature_visualization(model, img):
     conv_img = model(torch.Tensor(img_batch))
     conv_img = np.squeeze(conv_img, axis=0)
     conv_vector = conv_img.detach().numpy()
-    conv_img = conv_img.reshape((input_size, input_size)).detach().numpy()
-    cv2.imshow('conv', conv_img)
-    cv2.waitKey()
     return conv_vector
 
 
 feature_vector = feature_visualization(model, test)
-print(feature_vector)
